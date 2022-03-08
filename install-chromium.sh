40ad598d0896b2542f492f1a7e95329cd733049e
agray <agray4797@gmail.com>
2022-03-08 11:24:37 +0000

    selenium

---
+++ install-chromium.sh
@@ -0,0 +1,3 @@
+wget https://chromedriver.storage.googleapis.com/99.0.4844.51/chromedriver_linux64.zip
+sudo unzip chromedriver_linux64.zip -d /usr/bin
+rm chromedriver_linux64.zip
---
+++ tests/test_int.py
@@ -0,0 +1,49 @@
+from flask_testing import LiveServerTestCase
+from selenium import webdriver
+from urllib.request import urlopen
+from flask import url_for
+from datetime import date, timedelta
+
+from application import app, db
+from application.models import Todo, Project
+from application.forms import AddProject
+
+class TestBase(LiveServerTestCase):
+    TEST_PORT = 5050
+    def create_app(self):
+        app.config.update(
+            SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db',
+            LIVESERVER_PORT = 5050,
+            DEBUG = True,
+            TESTING = True
+        )
+        return app
+    
+    def setUp(self):
+        chrome_options = webdriver.chrome.options.Options()
+        chrome_options.add_argument('--headless')
+        self.driver = webdriver.Chrome(options = chrome_options)
+        db.create_all()
+        self.driver.get(f'http://localhost:{self.TEST_PORT}/create-proj')
+
+    def tearDown(self):
+        self.driver.quit()
+        db.drop_all()
+    
+    def test_server_running(self):
+        response = urlopen('http://localhost:5050/create-proj')
+        self.assert200(response)
+    
+class TestAddProject(TestBase):
+    TEST_CASES = ('Project 1', date.today() + timedelta(30)), ('Project 2', date.today() + timedelta(14))
+
+    def submit_input(self, case):
+        self.driver.find_element_by_xpath('html/body/div/form/input[2]').send_keys(case[0])
+        self.driver.find_element_by_xpath('html/body/div/form/input[3]').send_keys(case[1])
+        self.driver.find_element_by_xpath('html/body/div/form/input[4]').click()
+
+    def test_add_proj(self):
+        for case in self.TEST_CASES:
+            self.submit_input(case)
+            projs = Project.query.filter_by(project_name=case[0]).all()
+            self.assertNotEqual(projs, None)
